6439860df46846543eb50c677c9a5a749cbac95a
vm10-m <vinaymagan@hotmail.com>
2025-10-27 10:58:46 +0000

    Add files via upload

---
+++ MA1805/0.5 media/sketch.js
@@ -0,0 +1,15 @@
+let img ; 
+
+function preload() {
+    img = loadImage("Antenna.jpg");
+}
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+    background(220);
+    image (img,0,0,)
+    circle (50,50,50)
+}
